d121156fbddaf51d2f18b43752f44ed004d883a7
Robby Ciliberto <robby@ciliber.to>
2022-12-09 16:32:47 -0500

    Successful registration and redirection

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,13 +1,53 @@
-import type { Actions } from '@sveltejs/kit';
+import { invalid, redirect, type Actions } from '@sveltejs/kit';
+import { createUser, findUserByUsername } from '$lib/db/User';
 
 /** Form submission actions */
 export const actions: Actions = {
 	/** Action for registering the user */
-	default: async ({ request }) => {
+	default: async ({ request, cookies }) => {
+		// Get username and password from login
 		const data = await request.formData();
-		// TODO register the user
+		const username = data.get('username') as string;
+		const password = data.get('password') as string;
+		const verifyPassword = data.get('verifyPassword') as string;
 
-		console.log('User registered:', data.get('username'));
-		return { success: false };
+		// Reject is username is empty
+		if (!username) {
+			return invalid(400, { username, missing: true });
+		}
+
+		// Reject if password is empty
+		if (!password) {
+			return invalid(400, { password, missing: true });
+		}
+
+		// Reject if password is empty
+		if (!verifyPassword) {
+			return invalid(400, { verifyPassword, missing: true });
+		}
+
+		// Reject if password doesn't match verification
+		if (password != verifyPassword) {
+			return invalid(400, { password, invalid: true });
+		}
+
+		// Look for an already existing user
+		const existingUser = await findUserByUsername(username);
+
+		// Reject if there already exists a user with that username
+		if (existingUser) {
+			return invalid(401, { credentials: username, invalid: true });
+		}
+
+		// Create a new user
+		await createUser({
+			username: username,
+			password: password
+		});
+
+		// Log the user in by setting the currentUser cookie to the user's username
+		cookies.set('currentUser', username, { path: '/' });
+		console.log('User registered and logged in:', username);
+		throw redirect(302, '/home');
 	}
 };
