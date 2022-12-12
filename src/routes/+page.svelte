7917d65b3d78fb5b3f95c9b972be66e826a2d7bd
Theo Jolly <theonjolly@gmail.com>
2022-12-11 19:17:10 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,63 +1,39 @@
 <script src="../assets/dist/js/bootstrap.bundle.min.js">
 	// can also define functions here to be used for events (user clicks on something to take them to something)
-
 	import { onMount } from 'svelte';
-	let responseData;
+
+	function group(articles) {
+		return [
+			[articles[0], articles[1]],
+			[articles[2], articles[3]],
+			[articles[4], articles[5]],
+			[articles[6], articles[7]]
+		];
+	}
+
+	let featuredArticles;
 	onMount(async () => {
-		const response = await fetch(
-			'https://newsapi.org/v2/top-headlines?sortBy=popularity&language=en',
-			{
-				method: 'GET',
-				headers: {
-					'X-API-KEY': '2128dfda75c54ed3b045b635f45604e9'
-				}
+		// change endpoint depending on whether used is logged in or not and depending on their preferences?
+		let newsapiEndpoint = 'https://newsapi.org/v2/top-headlines?sortBy=popularity&language=en';
+		const response = await fetch(newsapiEndpoint, {
+			method: 'GET',
+			headers: {
+				'X-API-KEY': '2128dfda75c54ed3b045b635f45604e9'
 			}
-		)
+		})
 			.then((response) => response.json())
-			.then((data) => (responseData = data))
+			.then((data) => (featuredArticles = group(data.articles.slice(0, 8))))
 			.catch((error) => console.log(error));
 	});
 </script>
 
-<p>{responseData ?? 1}</p>
-
-<header class="site-header sticky-top py-1">
-	<nav class="container d-flex flex-column flex-md-row justify-content-between">
-		<a class="py-2" href="#" aria-label="Product">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="d-block mx-auto"
-				role="img"
-				viewBox="0 0 24 24"
-				><title>Product</title><circle cx="12" cy="12" r="10" /><path
-					d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"
-				/></svg
-			>
-		</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-		<a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
-	</nav>
-</header>
-
 <main>
 	<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 		<div class="col-md-5 p-lg-5 mx-auto my-5">
-			<h1 class="display-4 fw-normal">Punny headline</h1>
+			<h1 class="display-4 fw-normal">Welcome to Newzlife!</h1>
 			<p class="lead fw-normal">
-				And an even wittier subheading to boot. Jumpstart your marketing efforts with this example
-				based on Apple’s marketing pages.
+				Find information on the latest news around the world, and share insights and conversations
+				with your peers about them, all at the same time.
 			</p>
 			<a class="btn btn-outline-secondary" href="#">Coming soon</a>
 		</div>
@@ -65,184 +41,49 @@
 		<div class="product-device product-device-2 shadow-sm d-none d-md-block" />
 	</div>
 
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
+	{#if featuredArticles}
+		{#each featuredArticles as pairArticles}
+			<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+				<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+					<div class="my-3 p-3">
+						<a href={pairArticles[0].url} target="_blank">
+							<h2 class="font-weight-bold title-font">{pairArticles[0].title}</h2>
+						</a>
+						<p class="lead">{pairArticles[0].description}</p>
+					</div>
+					<img src={pairArticles[0].urlToImage} />
+				</div>
+				<div
+					class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"
+				>
+					<div class="my-3 py-3">
+						<a href={pairArticles[1].url} target="_blank">
+							<h2 class="font-weight-bold title-font">{pairArticles[1].title}</h2>
+						</a>
+						<p class="lead">{pairArticles[1].description}</p>
+					</div>
+					<img src={pairArticles[1].urlToImage} />
+				</div>
 			</div>
-			<div
-				class="bg-light shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-dark shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-dark shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-		<div
-			class="bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"
-		>
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-light shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-body shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-body shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-	</div>
-
-	<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 p-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-body shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-		<div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-			<div class="my-3 py-3">
-				<h2 class="display-5">Another headline</h2>
-				<p class="lead">And an even wittier subheading.</p>
-			</div>
-			<div
-				class="bg-body shadow-sm mx-auto"
-				style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-			/>
-		</div>
-	</div>
+		{/each}
+	{:else}
+		<p>Loading...</p>
+	{/if}
 </main>
 
-<footer class="container py-5">
-	<div class="row">
-		<div class="col-12 col-md">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				fill="none"
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				class="d-block mb-2"
-				role="img"
-				viewBox="0 0 24 24"
-				><title>Product</title><circle cx="12" cy="12" r="10" /><path
-					d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"
-				/></svg
-			>
-			<small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
-		</div>
-		<div class="col-6 col-md">
-			<h5>Features</h5>
-			<ul class="list-unstyled text-small">
-				<li><a class="link-secondary" href="#">Cool stuff</a></li>
-				<li><a class="link-secondary" href="#">Random feature</a></li>
-				<li><a class="link-secondary" href="#">Team feature</a></li>
-				<li><a class="link-secondary" href="#">Stuff for developers</a></li>
-				<li><a class="link-secondary" href="#">Another one</a></li>
-				<li><a class="link-secondary" href="#">Last time</a></li>
-			</ul>
-		</div>
-		<div class="col-6 col-md">
-			<h5>Resources</h5>
-			<ul class="list-unstyled text-small">
-				<li><a class="link-secondary" href="#">Resource name</a></li>
-				<li><a class="link-secondary" href="#">Resource</a></li>
-				<li><a class="link-secondary" href="#">Another resource</a></li>
-				<li><a class="link-secondary" href="#">Final resource</a></li>
-			</ul>
-		</div>
-		<div class="col-6 col-md">
-			<h5>Resources</h5>
-			<ul class="list-unstyled text-small">
-				<li><a class="link-secondary" href="#">Business</a></li>
-				<li><a class="link-secondary" href="#">Education</a></li>
-				<li><a class="link-secondary" href="#">Government</a></li>
-				<li><a class="link-secondary" href="#">Gaming</a></li>
-			</ul>
-		</div>
-		<div class="col-6 col-md">
-			<h5>About</h5>
-			<ul class="list-unstyled text-small">
-				<li><a class="link-secondary" href="#">Team</a></li>
-				<li><a class="link-secondary" href="#">Locations</a></li>
-				<li><a class="link-secondary" href="#">Privacy</a></li>
-				<li><a class="link-secondary" href="#">Terms</a></li>
-			</ul>
-		</div>
-	</div>
-</footer>
-
 <style>
-	.container {
-		max-width: 960px;
-	}
-
-	/*
-		 * Custom translucent site header
-		 */
-
-	.site-header {
-		background-color: rgba(0, 0, 0, 0.85);
-		-webkit-backdrop-filter: saturate(180%) blur(20px);
-		backdrop-filter: saturate(180%) blur(20px);
-	}
 	.site-header a {
 		color: #8e8e8e;
 		transition: color 0.15s ease-in-out;
 	}
+
 	.site-header a:hover {
 		color: #fff;
 		text-decoration: none;
+	}
+
+	.title-font {
+		font-size: 2.5em;
 	}
 
 	/*
