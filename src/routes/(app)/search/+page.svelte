1f8ec2b51c2a864a16958ed54521bd24fd489596
Theo Jolly <theonjolly@gmail.com>
2022-12-13 13:38:17 -0500

    Search page basis done (add details once done)

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -3,6 +3,7 @@
 	import { goto } from '$app/navigation';
 
 	let criteria = $page.url.searchParams.get('criteria');
+	let searchResults;
 
 	function search(givenCriteria) {
 		const searchUrl = new URL($page.url);
@@ -11,151 +12,176 @@
 		goto(searchUrl);
 	}
 
+	// group article results by 3 for display
+	function groupBy3(articles) {
+		let groupedArticles = [];
+		console.log(articles.length);
+		if (articles.length >= 3) {
+			for (let ii = 0; ii + 2 < articles.length; ii = ii + 3) {
+				groupedArticles.push([articles[ii], articles[ii + 1], articles[ii + 2]]);
+			}
+		} else {
+			// 2 or fewer articles; make a single array with them
+			let singleGroup = [];
+			articles.forEach((article) => singleGroup.push(article));
+			groupedArticles.push(singleGroup);
+		}
+		return groupedArticles;
+	}
+
 	// here, handle a search page w criteria (i.e. by hand or a search has been clicked)
 	$: if (criteria) {
+		let newsapiEndpoint = `https://newsapi.org/v2/everything?sortBy=relevancy&language=en&pageSize=60&q=${criteria}`;
+		fetch(newsapiEndpoint, {
+			method: 'GET',
+			headers: {
+				'X-API-KEY': '2128dfda75c54ed3b045b635f45604e9'
+			}
+		})
+			.then((response) => response.json())
+			.then((data) => (searchResults = groupBy3(data.articles)))
+			.catch((error) => console.log(error));
 	}
 </script>
 
-<div class="input-group">
-	<div class="form-outline">
-		<input id="search-input" type="search" class="form-control" />
-		<label class="form-label" for="search-input">Search</label>
+<section class="py-5 text-center container">
+	<h1 class="fw-light">Search NewzLife</h1>
+</section>
+
+<div class="container">
+	<br />
+	<div class="row justify-content-center">
+		<div class="col-12 col-md-10 col-lg-8">
+			<form class="card card-sm">
+				<div class="card-body row no-gutters align-items-center">
+					<div class="col-auto">
+						<i class="fas fa-search h4 text-body" />
+					</div>
+					<div class="col">
+						<input
+							class="form-control form-control-lg form-control-borderless"
+							type="search"
+							placeholder="Search topics or keywords"
+							id="search-input"
+						/>
+					</div>
+					<div class="col-auto">
+						<button
+							class="btn btn-lg btn-success"
+							type="button"
+							on:click={() => search(document.getElementById('search-input').value)}>Search</button
+						>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
-	<button
-		id="search-button"
-		type="button"
-		class="btn btn-primary"
-		on:click={() => search(document.getElementById('search-input').value)}
-	>
-		<i class="fas fa-search" />
-	</button>
 </div>
 
-{#if criteria}
-	<section class="py-5 text-center container">
-		<div class="row py-lg-5">
-			<div class="col-lg-6 col-md-8 mx-auto">
-				<h1 class="fw-light">Album example</h1>
-				<p class="lead text-muted">
-					Something short and leading about the collection below—its contents, the creator, etc.
-					Make it short and sweet, but not too short so folks don’t simply skip over it entirely.
-				</p>
-				<p>
-					<a href="#" class="btn btn-primary my-2">Main call to action</a>
-					<a href="#" class="btn btn-secondary my-2">Secondary action</a>
-				</p>
-			</div>
-		</div>
-	</section>
-
-	<div class="album py-5 bg-light">
-		<div class="container">
-			<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-				<div class="col">
-					<div class="card shadow-sm">
-						<svg
-							class="bd-placeholder-img card-img-top"
-							width="100%"
-							height="225"
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							aria-label="Placeholder: Thumbnail"
-							preserveAspectRatio="xMidYMid slice"
-							focusable="false"
-							><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text
-								x="50%"
-								y="50%"
-								fill="#eceeef"
-								dy=".3em">Thumbnail</text
-							></svg
-						>
-
-						<div class="card-body">
-							<p class="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+{#if criteria && searchResults}
+	<br />
+	{#if searchResults.length === 0}
+		<h2>Looks like your search didn't come up with anything...</h2>
+	{:else if searchResults.length === 1 && searchResults[0].length < 3}
+		{#if searchResults[0].length == 1}
+			<div class="container">
+				<div class="row row-cols-1 g-3">
+					<div class="col">
+						<div class="card shadow-sm">
+							<img src={searchResults[0][0].urlToImage} />
+							<div class="card-body">
+								<strong>{searchResults[0][0].description}</strong>
+								<div class="d-flex justify-content-between align-items-center">
+									<a href={searchResults[0][0].url} class="stretched-link" target="_blank" />
+									<p>{searchResults[0][0].description}</p>
+									<small class="text-muted">{searchResults[0][0].publishedAt.split('T')[0]}</small>
 								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="card shadow-sm">
-						<svg
-							class="bd-placeholder-img card-img-top"
-							width="100%"
-							height="225"
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							aria-label="Placeholder: Thumbnail"
-							preserveAspectRatio="xMidYMid slice"
-							focusable="false"
-							><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text
-								x="50%"
-								y="50%"
-								fill="#eceeef"
-								dy=".3em">Thumbnail</text
-							></svg
-						>
-
-						<div class="card-body">
-							<p class="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="card shadow-sm">
-						<svg
-							class="bd-placeholder-img card-img-top"
-							width="100%"
-							height="225"
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							aria-label="Placeholder: Thumbnail"
-							preserveAspectRatio="xMidYMid slice"
-							focusable="false"
-							><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text
-								x="50%"
-								y="50%"
-								fill="#eceeef"
-								dy=".3em">Thumbnail</text
-							></svg
-						>
-
-						<div class="card-body">
-							<p class="card-text">
-								This is a wider card with supporting text below as a natural lead-in to additional
-								content. This content is a little bit longer.
-							</p>
-							<div class="d-flex justify-content-between align-items-center">
-								<div class="btn-group">
-									<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-									<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-								</div>
-								<small class="text-muted">9 mins</small>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
+		{:else}
+			<div class="container">
+				<div class="row row-cols-1 row-cols-sm-2 g-3">
+					<div class="col">
+						<div class="card shadow-sm">
+							<img src={searchResults[0][0].urlToImage} />
+							<div class="card-body">
+								<strong>{searchResults[0][0].title}</strong>
+								<div class="d-flex justify-content-between align-items-center">
+									<a href={searchResults[0][0].url} class="stretched-link" target="_blank" />
+									<p>{searchResults[0][0].description}</p>
+									<small class="text-muted">{searchResults[0][0].publishedAt.split('T')[0]}</small>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card shadow-sm">
+							<img src={searchResults[0][1].urlToImage} />
+							<div class="card-body">
+								<strong>{searchResults[0][1].title}</strong>
+								<div class="d-flex justify-content-between align-items-center">
+									<a href={searchResults[0][0].url} class="stretched-link" target="_blank" />
+									<p>{searchResults[0][1].description}</p>
+									<small class="text-muted">{searchResults[0][1].publishedAt.split('T')[0]}</small>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		{/if}
+	{:else}
+		{#each searchResults as articleTrio}
+			<div class="album py-5 bg-light">
+				<div class="container">
+					<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+						<div class="col">
+							<div class="card shadow-sm">
+								<img src={articleTrio[0].urlToImage} />
+								<div class="card-body">
+									<strong>{articleTrio[0].title}</strong>
+									<div class="d-flex justify-content-between align-items-center">
+										<a href={articleTrio[0].url} class="stretched-link" target="_blank" />
+										<p>{articleTrio[0].description}</p>
+										<small class="text-muted">{articleTrio[0].publishedAt.split('T')[0]}</small>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col">
+							<div class="card shadow-sm">
+								<img src={articleTrio[1].urlToImage} />
+								<div class="card-body">
+									<strong>{articleTrio[1].title}</strong>
+									<div class="d-flex justify-content-between align-items-center">
+										<a href={articleTrio[1].url} class="stretched-link" target="_blank" />
+										<p>{articleTrio[1].description}</p>
+										<small class="text-muted">{articleTrio[1].publishedAt.split('T')[0]}</small>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col">
+							<div class="card shadow-sm">
+								<img src={articleTrio[2].urlToImage} />
+								<div class="card-body">
+									<strong>{articleTrio[2].title}</strong>
+									<div class="d-flex justify-content-between align-items-center">
+										<a href={articleTrio[2].url} class="stretched-link" target="_blank" />
+										<p>{articleTrio[2].description}</p>
+										<small class="text-muted">{articleTrio[2].publishedAt.split('T')[0]}</small>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		{/each}
+	{/if}
 {:else}{/if}
 
 <style>
