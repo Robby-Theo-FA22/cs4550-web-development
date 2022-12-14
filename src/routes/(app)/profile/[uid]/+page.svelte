8b2fbf0ec7f742a0f4807f87206fbb1b16ceeaa7
Robby Ciliberto <robert.ciliberto@gmail.com>
2022-12-13 20:43:24 -0500

    Finish Profile functionality
    Signed-off-by: Robby Ciliberto <robert.ciliberto@gmail.com>

--- src/routes/(app)/profile/[uid]/+page.svelte
+++ src/routes/(app)/profile/[uid]/+page.svelte
@@ -4,18 +4,17 @@
 	import ProfileDetail from '$lib/components/profile/ProfileDetail.svelte';
 
 	const user = $page.data.user;
-	$: socialInteraction = user.socialInteraction
+	$: socialInteraction = user.socialInteraction;
 
 	// currentUser cookie
 	$: currentUser = $page.data.currentUser;
 
 	const handleChangeSocialStatus = async () => {
-		console.log('change to', !socialInteraction)
 		let userUpdate = { 'socialInteraction': !socialInteraction };
 		const response = fetch(`/api/user/${user._id}`, {
 			method: 'POST',
 			body: JSON.stringify(userUpdate)
-		})
+		});
 
 		if ((await response).ok) {
 			socialInteraction = !socialInteraction;
@@ -24,51 +23,57 @@
 </script>
 
 <h1>{user.username}'s Profile</h1>
+<h3>User Type: {socialInteraction ? 'Social User' : 'Lurker'}</h3>
 <hr>
 
-<h3>Comments</h3>
-{#each user.comments as cid}
-	<ProfileComment commentId={cid} />
-{:else }
-	<p>Nothing here...</p>
-{/each}
+{#if socialInteraction}
+	<h3>Comments</h3>
+	{#each user.comments as cid}
+		<ProfileComment commentId={cid} />
+	{:else }
+		<p>Nothing here...</p>
+	{/each}
+
+	{#if currentUser === user.username}
+		<hr />
+		<div class='mb-3'>
+			<h3>Your Bookmarks</h3>
+			{#each user.bookmarks as did}
+				<ProfileDetail detailId={did} />
+			{:else}
+				<p>Nothing here...</p>
+			{/each}
+		</div>
+
+
+		<div class='mb-3'>
+			<h3>Your Liked Articles</h3>
+			{#each user.likedDetails as did}
+				<ProfileDetail detailId={did} />
+			{:else}
+				<p>Nothing here...</p>
+			{/each}
+		</div>
+
+		<div class='mb-3'>
+			<h3>Your Liked Comments</h3>
+			{#each user.likedComments as cid}
+				<ProfileComment commentId={cid} />
+			{:else}
+				<p>Nothing here...</p>
+			{/each}
+		</div>
+	{/if}
+	<hr />
+{/if}
 
 {#if currentUser === user.username}
-	<hr />
-	<div class='mb-3'>
-		<h3>Your Bookmarks</h3>
-		{#each user.bookmarks as did}
-			<ProfileDetail detailId={did} />
-		{:else}
-			<p>Nothing here...</p>
-		{/each}
-	</div>
-
-
-	<div class='mb-3'>
-		<h3>Your Liked Articles</h3>
-		{#each user.likedDetails as did}
-			<ProfileDetail detailId={did} />
-		{:else}
-			<p>Nothing here...</p>
-		{/each}
-	</div>
-
-	<div class='mb-3'>
-		<h3>Your Liked Comments</h3>
-		{#each user.likedComments as cid}
-			<ProfileComment commentId={cid} />
-		{:else}
-			<p>Nothing here...</p>
-		{/each}
-	</div>
-	<hr />
-
 	<h2>Settings</h2>
 	<div>
-		<h3>User Type: {socialInteraction ? 'Social User' : 'Lurker'}</h3>
 		<button class='btn btn-outline-success mb-3' on:click={handleChangeSocialStatus}>Change
 			to {socialInteraction ? 'Lurker' : 'Social User' }</button>
 	</div>
 {/if}
+
+
 
